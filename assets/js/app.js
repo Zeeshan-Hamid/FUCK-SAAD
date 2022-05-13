112a020e23615a4038bd2d8de63e2cc3f24d6ef2
Zeeshan Hamid <zeeshanhamid17@gmail.com>
2022-05-13 23:57:56 +0500

    second commit

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+const navMenu = document.getElementById('nav-menu'),
+toggleMenu = document.getElementById('toggle-menu'),
+closeMenu = document.getElementById('close-menu')
+ 
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show');
+})
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show');
+})
+
+
+const butn = document.querySelectorAll('a');
+    butn.forEach(btn => {
+        butn.addEventListener('click', function(e){
+    let x= e.clientX - e.target.offsetLeft;
+    let y = e.clientY - e.target.offsetTop;
+
+    let ripples = document.createElement('span');
+    ripples.style.left = x + 'px';
+    ripples.style.top = y + 'px';
+    this.appendChild(ripples);
+
+
+})
+    })
